2e15c1e3209492b83801f87e979215d9b08c99cb
josdic1 <joshuadicker@gmail.com>
2025-01-14 11:13:44 -0500

    completed module

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+   addingEventListener()
+})
+
+
 function addingEventListener() {
+   const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+
+
